8fe0185732f950ef7dbaef8f3ecf4f951a107073
Sarim Khan <sarim2005@gmail.com>
2017-01-03 02:18:40 +0600

    cleanup

--- ibus/factory.go
+++ ibus/factory.go
@@ -15,29 +15,28 @@ func NewFactory(conn *dbus.Conn, EngineCreator func(conn *dbus.Conn, engineName 
 	return f
 }
 
-/*
-# Return a array. [name, default_language, icon_path, authors, credits]
-@method(out_signature="as")
-def GetInfo(self): pass
+// # Return a array. [name, default_language, icon_path, authors, credits]
+// @method(out_signature="as")
+// def GetInfo(self): pass
 
-# Factory should allocate all resources in this method
-@method()
-def Initialize(self): pass
+// # Factory should allocate all resources in this method
+// @method()
+// def Initialize(self): pass
 
-# Factory should free all allocated resources in this method
-@method()
-def Uninitialize(self): pass
+// # Factory should free all allocated resources in this method
+// @method()
+// def Uninitialize(self): pass
 
-# Create an input context and return the id of the context.
-# If failed, it will return "" or None.
-@method(in_signature="s", out_signature="o")
-def CreateEngine(self, engine_name): pass
-
-# Destroy the engine
-@method()
-def Destroy(self): pass
-*/
-
+// # Create an input context and return the id of the context.
+// # If failed, it will return "" or None.
+// @method(in_signature="s", out_signature="o")
 func (factory *Factory) CreateEngine(engineName string) (dbus.ObjectPath, *dbus.Error) {
 	return factory.EngineCreator(factory.conn, engineName), nil
+}
+
+// # Destroy the engine
+// @method()
+func (factory *Factory) Destroy() *dbus.Error {
+	factory.conn.Export(nil, "/org/freedesktop/IBus/Factory", IBUS_IFACE_ENGINE_FACTORY)
+	return nil
 }
